0a50179dc364ca58b5e419013dfd9b8ee615f023
ritikraj07 <ritikra3rrr@gmail.com>
2024-04-05 15:59:02 +0530

    new file added

--- Router/file.router.js
+++ Router/file.router.js
@@ -1,23 +1,33 @@
 const Router = require("express").Router();
 const path = require("path");
 const fs = require("fs");
-const SendMail = require("../Middleware/sendMail");
-let count = 12;
+
+// Initialize count object to track downloads
+let count = {
+    "EPS-Main": 35,    // Initial count for "EPS-Main" file
+    "EPS-Actual": 0    // Initial count for "EPS-Actual" file
+}
+
+// Define route handler for GET requests to "/"
 Router.get("/", async (req, res) => {
     const { file } = req.query;
 
+    // Check if 'file' parameter is provided
     if (!file) {
         return res.status(400).send("File name is required.");
     }
 
+    // Resolve file path
     const filePath = path.resolve(`./Questions/${file}.pdf`);
 
     // Check if the file exists
     if (!fs.existsSync(filePath)) {
         return res.status(404).send("File not found.");
     }
-    count++;
-    console.log("Downloaded", count, "times");
+
+    // Increment download count for the requested file
+    count[file]++;
+    console.log(`Downloaded ${count} times`);
 
     // Set the Content-Disposition header to prompt download with the specified file name
     res.setHeader('Content-Disposition', `attachment; filename="${file}.pdf"`);
