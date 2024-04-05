f3f2cec1ec2326505b5031fcce578cf8a1bd10c1
ritikraj07 <ritikra3rrr@gmail.com>
2024-04-05 12:36:02 +0530

    sd

--- Script/mail.js
+++ Script/mail.js
@@ -28,92 +28,35 @@ class Mail {
         };
     }
 
-    /**
-     * Sets the name of the company that is sending the email.
-     *
-     * @param {string} name - The name of the company.
-     */
     setCompanyName(name) {
-        // Set the name of the company in the mail options.
         this.mailOptions.from.name = name;
     }
 
-    /**
-     * Sets the email address of the sender.
-     *
-     * @param {string} email - The email address of the sender.
-     */
     setSenderEmail(email) {
-        // Sets the email address of the sender in the mail options.
-        //
-        // @param {string} email - The email address of the sender.
         this.mailOptions.from.address = email;
     }
 
-    /**
-     * Sets the recipient email address.
-     *
-     * @param {string} receiver - The email address of the recipient.
-     */
     setTo(receiver) {
-        /**
-         * The email address of the recipient.
-         * @type {string}
-         */
         this.mailOptions.to = receiver;
     }
-    /**
-     * Sets the subject of the email.
-     *
-     * @param {string} subject - The subject of the email.
-     */
+
     setSubject(subject) {
-        // Sets the subject of the email.
-        // 
-        // @param {string} subject - The subject of the email.
         this.mailOptions.subject = subject;
     }
-    /**
-     * Sets the text content of the email.
-     *
-     * @param {string} text - The text content of the email.
-     */
+
     setText(text) {
-        /**
-         * The text content of the email.
-         * @type {string}
-         */
         this.mailOptions.text = text;
     }
 
-    /**
-     * Sets the HTML content of the email.
-     *
-     * @param {string} html - The HTML content of the email.
-     */
     setHTML(html) {
-        /**
-         * The HTML content of the email.
-         * @type {string}
-         */
         this.mailOptions.html = html;
     }
 
-    /**
-     * Sends the email using the configured options.
-     *
-     * This function uses the nodemailer transporter to send the email.
-     * If an error occursres during sending, it logs the error.
-     * If the email is sent successfully, it logs the response from the server.
-     *
-     * @return {void} Does not return anything.
-     */
     send() {
         try {
             transporter.sendMail(this.mailOptions, (error, info) => {
                 if (error) {
                     console.error('Error sending email:', error);
-                    reject(error);
                 } else {
                     console.log('Email sent successfully:', info.response);
                 }
@@ -123,8 +66,6 @@ class Mail {
             throw error; // Re-throw the error for the caller to handle
         }
     }
-
-
 }
 
 module.exports = Mail;
